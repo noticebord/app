3dc6e5b08a4aaf5c9ded46cd8a117ad7836b82ca
sixpeteunder <plenjo15@gmail.com>
2022-04-13 17:11:17 +0300

    Implement topic details page

--- lib/client/services/topic_service.dart
+++ lib/client/services/topic_service.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
 import 'package:app/client/models/list_notice.dart';
-import 'package:app/client/models/notice.dart';
+import 'package:app/client/models/paginated_list.dart';
 import 'package:app/client/models/topic.dart';
 import 'package:http/http.dart' as http;
 
 import 'service.dart';
 
-class TopicService extends Service{
-  TopicService(String? token, String baseUrl): super(token, baseUrl);
+class TopicService extends Service {
+  TopicService(String? token, String baseUrl) : super(token, baseUrl);
 
   Future<List<Topic>> fetchTopics() async {
     final response = await http.get(Uri.parse("$baseUrl/topics"),
@@ -36,13 +36,17 @@ class TopicService extends Service{
     }
   }
 
-  Future<List<ListNotice>> fetchTopicNotices(int topicId) async {
-    final response = await http.get(Uri.parse("$baseUrl/topics/$topicId/notices"),
+  Future<PaginatedList<ListNotice>> fetchTopicNotices(
+    int topicId, {
+    String? cursor,
+  }) async {
+    final response = await http.get(
+        Uri.parse("$baseUrl/topics/$topicId/notices?cursor=$cursor"),
         headers: Service.defaultHeaders);
 
     if (response.statusCode == 200) {
-      final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
-      return parsed.map<Notice>((item) => Notice.fromJSON(item)).toList();
+      final parsed = jsonDecode(response.body);
+      return PaginatedList<ListNotice>.fromJSON(parsed);
     } else {
       throw Exception(
           "Failed to fetch notices - ${response.statusCode}: ${response.reasonPhrase}");
--- lib/pages/topic_details_page.dart
+++ lib/pages/topic_details_page.dart
@@ -1,6 +1,13 @@
+import 'package:animations/animations.dart';
 import 'package:app/application_model.dart';
+import 'package:app/client/models/list_notice.dart';
+import 'package:app/client/models/paginated_list.dart';
 import 'package:app/client/models/topic.dart';
+import 'package:app/client/noticebord_client.dart';
+import 'package:app/pages/notice_details_page.dart';
+import 'package:app/widgets/list_notice_widget.dart';
 import 'package:app/widgets/loader_widget.dart';
+import 'package:app/widgets/loading_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,44 +20,114 @@ class TopicDetailsPage extends StatefulWidget {
 }
 
 class _TopicDetailsPageState extends State<TopicDetailsPage> {
-  late Future<Topic> futureTopic;
+  bool loading = false;
+  late NoticebordClient client;
+
+  late Topic topic;
+  late PaginatedList<ListNotice> lastResponse;
+  late List<ListNotice> topicNotices;
+
+  late Future futureTopicDetails;
+
+  Future fetchTopic(int topicId) async {
+    topic = await client.topics.fetchTopic(topicId);
+  }
+
+  Future fetchTopicNotices(int topicId) async {
+    lastResponse = await client.topics.fetchTopicNotices(topicId);
+    topicNotices = lastResponse.data;
+  }
 
   @override
   void initState() {
     super.initState();
-    final client = Provider.of<ApplicationModel>(context, listen: false).client;
-    futureTopic = client.topics.fetchTopic(widget.topicId);
+    final topicId = widget.topicId;
+
+    client = Provider.of<ApplicationModel>(context, listen: false).client;
+    futureTopicDetails =
+        Future.wait<void>([fetchTopic(topicId), fetchTopicNotices(topicId)]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Topic Details")),
-      body: FutureBuilder<Topic>(
-        future: futureTopic,
+      body: FutureBuilder(
+        future: futureTopicDetails,
         builder: (context, snapshot) {
           if (snapshot.connectionState != ConnectionState.done &&
               !snapshot.hasData) {
             return const LoaderWidget();
           }
 
-          final topic = snapshot.data!;
-          return ListView(
-            children: [
-              MaterialBanner(
-                content: Text("#${topic.name} - ${topic.count} notices"),
-                actions: [
-                  TextButton(
-                    onPressed: null,
-                    child: Row(children: const [
-                      Icon(Icons.add),
-                      SizedBox(width: 4.0),
-                      Text("New Notice"),
-                    ]),
-                  )
-                ],
-              )
-            ],
+          final itemCount = lastResponse.nextPageUrl == null
+              ? topicNotices.length + 1
+              : topicNotices.length + 2;
+          return ListView.builder(
+            itemCount: itemCount,
+            itemBuilder: (context, position) {
+              if (position == 0) {
+                return MaterialBanner(
+                  content: Text("#${topic.name} - ${topic.count} notices"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: const [
+                          Icon(Icons.add),
+                          SizedBox(width: 4.0),
+                          Text("Create New"),
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              }
+
+              if (position < topicNotices.length + 1) {
+                print(position);
+                final notice = topicNotices[position - 1];
+                return OpenContainer<bool>(
+                  transitionType: ContainerTransitionType.fade,
+                  openBuilder: (context, openContainer) {
+                    return NoticeDetailsPage(noticeId: notice.id);
+                  },
+                  tappable: false,
+                  closedShape: const RoundedRectangleBorder(),
+                  closedElevation: 0,
+                  closedBuilder: (context, openContainer) {
+                    return ListNoticeWidget(
+                      listNotice: notice,
+                      onTap: openContainer,
+                    );
+                  },
+                );
+              }
+
+              return Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 16.0),
+                child: LoadingButtonWidget(
+                  loading: loading,
+                  elevated: false,
+                  child: const Text("Load more"),
+                  onPressed: () async {
+                    setState(() => loading = true);
+
+                    final cursor =
+                    Uri.parse(lastResponse.nextPageUrl!)
+                        .queryParameters['cursor'];
+                    lastResponse = await client.topics
+                        .fetchTopicNotices(topic.id, cursor: cursor);
+
+                    setState(() {
+                      topicNotices.addAll(lastResponse.data);
+                      loading = false;
+                    });
+                  },
+                ),
+              );
+            },
           );
         },
       ),
