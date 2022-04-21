2c6d0ff5819a0eeeb06b306031d862b474c62589
sixpeteunder <plenjo15@gmail.com>
2022-04-21 17:54:37 +0300

    Scaffold out new notice page

--- lib/pages/new_notice_page.dart
+++ lib/pages/new_notice_page.dart
@@ -7,14 +7,48 @@ class NewNoticePage extends StatefulWidget {
   State<StatefulWidget> createState() => _NewNoticePageState();
 }
 
-class _NewNoticePageState extends State<NewNoticePage> {
+class _NewNoticePageState extends State<NewNoticePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int tabIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(initialIndex: 0, length: 2, vsync: this);
+    _tabController.addListener(() {
+      tabIndex = _tabController.index;
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('New Notice')
+        title: const Text('Create Notice'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.save))
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: false,
+          tabs: [
+            const Tab(text: 'Notice'),
+            const Tab(text: 'Team Notice'),
+          ],
+        ),
       ),
-      body: const Center(child: Text('I will create a new notice here.')),
+      body: TabBarView(controller: _tabController, children: [
+        const Center(child: Text('Create notices.')),
+        const Center(child: Text('Create team notices.')),
+      ],)
+
     );
   }
 }
